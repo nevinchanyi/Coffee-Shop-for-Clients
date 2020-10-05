5d431cdd941991e46fa2ebd51749c5897271d816
Kostya Nevinchanyi <constantinekos9@gmail.com>
2020-10-05 12:48:43 +0300

    Initial Commit

---
+++ Coffee Shop/Coffee_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_ShopApp.swift
+//  Coffee Shop
+//
+//  Created by constantine kos on 05.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
